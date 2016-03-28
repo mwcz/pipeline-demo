f920cf303b362736e6aea5138ff5570b43270436
mwcz <mwc@clayto.com>
2016-03-28 15:03:21 -0400

    move particles in a line towards click point

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ var scene;
 var renderer;
 var clock;
 var timescale;
+var stats;
 var particles = {};
 
 var width = window.innerWidth;
@@ -49,6 +50,11 @@ function init() {
     camera.position.z = 1000;
 
     clock = new THREE.Clock();
+    stats = new Stats();
+    stats.domElement.style.position = 'absolute';
+    stats.domElement.style.bottom = '0px';
+    stats.domElement.style.right = '0px';
+    document.body.appendChild( stats.domElement );
 
     scene = new THREE.Scene();
 
@@ -75,19 +81,23 @@ function init() {
 
 function initParticles(particles) {
     particleGroup = new SPE.Group({
-        maxParticleCount: 2000,
+        maxParticleCount: 1000,
+        valueOverLifetimeLength: 4,
+        transparent: true,
         texture: {
             value: THREE.ImageUtils.loadTexture('./img/traffic-dot.png')
-        }
+        },
     });
 
     emitter = new SPE.Emitter({
+        direction: -1,
         maxAge: {
-            value: 4
+            value: 1
         },
         position: {
             value: new THREE.Vector3(0, 0, 0),
-            spread: new THREE.Vector3( 0, 0, 0 )
+            // spread: new THREE.Vector3( 0, 0, 0 ),
+            randomise: true,
         },
 
         // acceleration: {
@@ -95,21 +105,26 @@ function initParticles(particles) {
         //     spread: new THREE.Vector3( 100, 0, 10 )
         // },
 
+        opacity: {
+            value: [1, 1, 1, 1]
+        },
+
         velocity: {
-            value: new THREE.Vector3(0, 250, 0),
-            spread: new THREE.Vector3(100, 70.5, 0),
+            value: new THREE.Vector3(0, 20, 0),
+            // spread: new THREE.Vector3(0, 0, 0),
             randomise: true,
         },
 
         color: {
-            value: [ new THREE.Color(0x92d400), new THREE.Color(0x7cdbf3), new THREE.Color(0xf0ab00) ]
+            value: [  new THREE.Color(0xf0ab00), new THREE.Color(0x92d400) ],
+            randomise: true,
         },
 
         size: {
-            value: 100
+            value: [65,65,65,65],
         },
 
-        particleCount: 2000,
+        particleCount: 100,
     });
 
     setInterval( function() { particleGroup.mesh.position.copy( getInitialPosition() ); particleGroup.mesh.position.z = 1; }, 10);
@@ -118,13 +133,32 @@ function initParticles(particles) {
     scene.add( particleGroup.mesh );
 }
 
-function onClick(evt) {
-    var coords = toWorldCoords( new THREE.Vector2(evt.clientX, evt.clientY) );
-    emitter.velocity.value.copy(coords.sub(particleGroup.mesh.position));
-    // pinToFrustum(particleGroup.mesh, coords);
+function centerPoint(el) {
+    var coords = el.getBoundingClientRect();
+    return {
+        x: ( coords.left + coords.right ) / 2,
+        y: ( coords.top + coords.bottom ) / 2,
+    };
 }
+
+function onClick(evt) {
+    var coords_fixed =  toWorldCoords(new THREE.Vector2( 0, 0 ));
+    var coords = toWorldCoords( new THREE.Vector2(evt.clientX, evt.clientY) );
+    emitter.velocity.value.copy(coords_fixed);
+    emitter.position.value.copy(coords.sub(particleGroup.mesh.position));
+
+    emitter.color.value[0] = new THREE.Color( getBackgroundColor( evt.target ) );
+    // pinToFrustum(coords.sub(particleGroup.mesh.position), coords);
+}
+
+function getBackgroundColor(element) {
+    return window.getComputedStyle( element ).backgroundColor;
+}
+
 function getInitialPosition() {
-    var coords2d =  new THREE.Vector2( width * 0.77, height * 0.88 );
+    var blueCenter = centerPoint(document.querySelector('.commit-blue'));
+    var coords2d =  new THREE.Vector2( blueCenter.x, blueCenter.y );
+    // var coords2d =  new THREE.Vector2( width * 0.77, height * 0.08 );
     var coords3d = toWorldCoords(coords2d);
     return coords3d;
 }
@@ -156,6 +190,7 @@ function onWindowResize() {
 
 function animate() {
 
+    stats.update();
     requestAnimationFrame( animate );
 
     timescale = clock.getDelta();
